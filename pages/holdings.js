5e319523fc1ea4cbe35f1dd0853e5ab52d20ba4d
Devjyoti Ghosh <devjyotighosh02@gmail.com>
2020-08-12 18:48:51 +0530

    Fetched data from holdings api

--- pages/holdings.js
+++ pages/holdings.js
@@ -1,9 +1,23 @@
+import { useQuery } from "react-query";
+import Typography from "@material-ui/core/Typography";
+import { getHoldings } from "../src/api/getCanopyApi";
 import MainLayout from "../src/components/layout/MainLayout";
 
-export default function index() {
+export default function holdings(props) {
+  const { isLoading, error, data } = useQuery("holdingsData", getHoldings, {
+    initialData: props.holdings,
+  });
   return (
     <>
-      <MainLayout />
+      <MainLayout>
+        <Typography variant="h4" component="h2" gutterBottom>
+          Holdings Table
+        </Typography>
+      </MainLayout>
     </>
   );
 }
+export const getStaticProps = async () => {
+  const holdings = await getHoldings();
+  return { props: { holdings } };
+};
